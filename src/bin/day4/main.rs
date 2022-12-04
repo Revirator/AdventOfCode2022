6f83f93b08d1fe6cd795f72429ae679674dad26b
Denis Tsvetkov <D.R.Tsvetkov@student.tudelft.nl>
2022-12-04 13:28:34 +0100

    update day 4 part 2

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -9,17 +9,16 @@ fn main() {
                 let split: Vec<&str> = ip.split(',').collect();
                 let elf_1: Vec<&str> = split[0].split('-').collect();
                 let elf_2: Vec<&str> = split[1].split('-').collect();
-                let (s_11, s_12, s_21, s_22) = (
+                let (a_11, a_12, a_21, a_22) = (
                     &elf_1[0].parse::<u32>().unwrap(),
                     &elf_1[1].parse::<u32>().unwrap(),
                     &elf_2[0].parse::<u32>().unwrap(),
                     &elf_2[1].parse::<u32>().unwrap()
                 );
-                if (s_11 >= s_21 && s_12 <= s_22) || (s_21 >= s_11 && s_22 <= s_12) {
+                if (a_11 >= a_21 && a_12 <= a_22) || (a_21 >= a_11 && a_22 <= a_12) {
                     complete_overlap += 1;
                 }
-                if (s_11 >= s_21 && s_11 <= s_22) || (s_12 >= s_21 && s_12 <= s_22)
-                    || (s_21 >= s_11 && s_21 <= s_12) || (s_22 >= s_11 && s_22 <= s_12) {
+                if a_12 >= a_21 && a_22 >= a_11 {
                     partial_overlap += 1;
                 }
             }
